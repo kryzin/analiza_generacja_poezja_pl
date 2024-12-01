6aa40fec3776085def03121d3d55726f4f360020
Karolina Ryzińska <76969276+kryzin@users.noreply.github.com>
2024-12-01 22:48:29 +0100

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from transformers import pipeline
-import torch
 from datasets import Dataset
 
-csv_file = "cleaned_poems.csv"  # Replace with your CSV filename
+
+csv_file = "cleaned_poems.csv"
 df = pd.read_csv(csv_file)
 print(f"The DataFrame now has {len(df)} rows.")
-# Comprehensive candidate labels
+
 candidate_labels = [
     "smutek", "miłość", "natura", "zemsta",
     "wojna", "wolność", "radość",
@@ -20,15 +20,15 @@ dataset = Dataset.from_pandas(df)
 classifier = pipeline(
     "zero-shot-classification",
     model="MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7",
-    device=0,  # Use GPU
-    batch_size=16  # Process 8 poems at a time
+    device=0,
+    batch_size=16
 )
 
-# Function to classify a batch of texts
+
 def classify_batch(batch):
-    texts = [text for text in batch["Content"] if text]  # Filter out None or empty texts
+    texts = [text for text in batch["Content"] if text]
     if not texts:
-        return {"Labels": []}  # Return empty labels if no valid text
+        return {"Labels": []}
 
     results = classifier(
         texts,
@@ -37,10 +37,10 @@ def classify_batch(batch):
         truncation=True
     )
 
-    # Extract labels with scores > 0.75 for each text
     batch["Labels"] = [
         ", ".join(
-            label for label, score in zip(result["labels"], result["scores"]) if score > 0.93
+            label for label, score in zip(result["labels"],
+                                          result["scores"]) if score > 0.93
         )
         for result in results
     ]
@@ -48,12 +48,6 @@ def classify_batch(batch):
 
 
 classified_dataset = dataset.map(classify_batch, batched=True, batch_size=8)
-
-# Convert back to a pandas DataFrame
 classified_df = classified_dataset.to_pandas()
-
-# Save the results to a new CSV file
 classified_df.to_csv("classified_poems.csv", index=False, encoding="utf-8")
-
 print("Classification completed. Results saved to 'classified_poems.csv'.")
-
